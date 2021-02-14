5b354b83c8f6a927a1fe3c3a0c03de69cc06a56a
naberna <bernaa13@gmail.com>
2021-02-14 22:04:10 +0300

    load data from backend and show them in frontend

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,14 @@ import data from './data.js';
 
 const app = express();
 
-app.get('/api/products', (req, res) =>{
-    res.send(data.products);
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
 });
 
 app.get('/', (req, res) => {
-    res.send('Server is ready')
+  res.send('Server is ready');
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
