67f03cb6b6c11f087cc6ec08b9b4c892384d620a
naberna <bernaa13@gmail.com>
2021-02-25 19:22:15 +0300

    mongod connection and sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+      {
+        name: 'Berna',
+        email: 'berna@bernapolat.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: true
+      },
+      {
+        name: 'John',
+        email: 'user@bernapolat.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false
+      }
+    ],
     products: [
       {
         _id: '1',
