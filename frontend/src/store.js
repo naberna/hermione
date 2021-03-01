6ec0fd1a6613bf45510fa21f64cf5b2709361a16
naberna <bernaa13@gmail.com>
2021-03-01 13:38:08 +0300

    Checkout Steps-Address, Payment and Place Order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,29 @@
-import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { cartReducer } from './reducers/cartReducers';
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 import {
   productDetailsReducer,
   productListReducer,
-} from './reducers/productReducers';
+} from "./reducers/productReducers";
 import {
   userRegisterReducer,
   userSigninReducer,
-} from './reducers/userReducers';
+} from "./reducers/userReducers";
 
 const initialState = {
   userSignin: {
-    userInfo: localStorage.getItem('userInfo')
-      ? JSON.parse(localStorage.getItem('userInfo'))
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
       : null,
   },
   cart: {
-    cartItems: localStorage.getItem('cartItems')
-      ? JSON.parse(localStorage.getItem('cartItems'))
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+    paymentMethod: 'PayPal',
   },
 };
 const reducer = combineReducers({
